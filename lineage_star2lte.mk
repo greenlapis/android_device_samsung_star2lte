70d584cb3736de6c1b93e3762de6af0b7f831586
ausis-projects <studentausis@gmail.com>
2026-09-14 14:44:17 +0600

    Update lineage_star2lte.mk

--- lineage_star2lte.mk
+++ lineage_star2lte.mk
@@ -12,7 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, device/samsung/star2lte/device.mk)
 
 # Inherit some common Lineage stuff
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Device identifier, this must come after all inclusions
 PRODUCT_NAME := lineage_star2lte
@@ -22,3 +26,13 @@ PRODUCT_MODEL := SM-G965F
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+# Axion flags
+AXION_MAINTAINER := bobo
+AXION_PROCESSOR := exynos 9810 
+TARGET_ENABLE_BLUR := false
+TARGET_INCLUDE_AXFX := true
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="samsung star2lte star2lte:16 BP4A.251205.006 eng.androi:user release-keys" \
+    BuildFingerprint=samsung/star2lte/star2lte:16/BP4A.251205.006/eng.androi:user/release-keys
